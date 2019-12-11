f1c54a4ed7ac104bcfaceb724b4348b9c21f9024
Toumette <58706169+Toumette@users.noreply.github.com>
2019-12-11 21:07:54 +0100

    Add files via upload

---
+++ BackupWin.bat
@@ -0,0 +1,40 @@
+@echo off
+REM HC-12/04/10
+echo ===============================================
+echo Sauvegarde recursive incrementale de la cle USB "NomCle"
+echo dans le repertoire "X:\SauveCles\NomCle"
+echo "X:" est la lettre du disque recevant la sauvegarde
+echo ne sauve que les fichiers nouveaux ou + recents
+echo ===============================================
+REM Script à placer à la racine de la clé USB
+REM fonctionne en environnement DOS / WINDOWS
+echo -----------------------------------------------
+echo Datation de la sauvegarde dans le fichier
+echo historique "histo.txt" :
+date /T
+echo -----------------------------------------------
+date /T >> .\histo.txt
+echo -----------------------------------------------
+echo debut sauvegarde
+echo -----------------------------------------------
+REM il faut mettre le caractère d'échappement (^)
+REM juste après le dernier caractère de la ligne et
+REM reporter l'espace en début de ligne suivante :
+XCOPY "."^
+ "D:\SauveCles\NomCle\"^
+ /C /E /D /R /H /Y
+echo -----------------------------------------------
+REM      OPTIONS DE XCOPY UTILISEES :
+REM /C : Continue la copie même si des erreurs se produisent
+REM /E : Copie récursive incluant les répertoires vides
+REM /D : copie uniquement les fichiers dont l'heure source
+REM      est plus récente que l'heure de destination
+REM /R : copie aussi les fichiers en lecture seule
+REM /H : Copie aussi les fichiers cachés et les fichiers système
+REM /Y : Supprime la demande de confirmation de remplacement
+REM      des fichiers de destination existants
+echo -----------------------------------------------
+echo SAUVEGARDE TERMINEE
+echo -----------------------------------------------
+REM attente d'appui sur une touche pour fermer la fenetre :
+pause
