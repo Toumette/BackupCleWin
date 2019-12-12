3bded658b173b4ccd4bb49f80004253435f253e9
Toumette <58706169+Toumette@users.noreply.github.com>
2019-12-12 19:11:14 +0100

    Update BackupWin.bat

--- BackupWin.bat
+++ BackupWin.bat
@@ -1,12 +1,12 @@
 @echo off
-REM HC-12/04/10
+REM (c) Henri 12/04/10
 echo ===============================================
 echo Sauvegarde recursive incrementale de la cle USB "NomCle"
 echo dans le repertoire "X:\SauveCles\NomCle"
 echo "X:" est la lettre du disque recevant la sauvegarde
 echo ne sauve que les fichiers nouveaux ou + recents
 echo ===============================================
-REM Script à placer à la racine de la clé USB
+REM Script Ã  placer Ã  la racine de la clÃ© USB
 REM fonctionne en environnement DOS / WINDOWS
 echo -----------------------------------------------
 echo Datation de la sauvegarde dans le fichier
@@ -17,20 +17,20 @@ date /T >> .\histo.txt
 echo -----------------------------------------------
 echo debut sauvegarde
 echo -----------------------------------------------
-REM il faut mettre le caractère d'échappement (^)
-REM juste après le dernier caractère de la ligne et
-REM reporter l'espace en début de ligne suivante :
+REM il faut mettre le caractÃ¨re d'Ã©chappement (^)
+REM juste aprÃ¨s le dernier caractÃ¨re de la ligne et
+REM reporter l'espace en dÃ©but de ligne suivante :
 XCOPY "."^
  "D:\SauveCles\NomCle\"^
  /C /E /D /R /H /Y
 echo -----------------------------------------------
 REM      OPTIONS DE XCOPY UTILISEES :
-REM /C : Continue la copie même si des erreurs se produisent
-REM /E : Copie récursive incluant les répertoires vides
+REM /C : Continue la copie mÃªme si des erreurs se produisent
+REM /E : Copie rÃ©cursive incluant les rÃ©pertoires vides
 REM /D : copie uniquement les fichiers dont l'heure source
-REM      est plus récente que l'heure de destination
+REM      est plus rÃ©cente que l'heure de destination
 REM /R : copie aussi les fichiers en lecture seule
-REM /H : Copie aussi les fichiers cachés et les fichiers système
+REM /H : Copie aussi les fichiers cachÃ©s et les fichiers systÃ¨me
 REM /Y : Supprime la demande de confirmation de remplacement
 REM      des fichiers de destination existants
 echo -----------------------------------------------
